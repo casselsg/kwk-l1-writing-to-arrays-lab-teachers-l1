ea8f031edb92b0806286b01adfea27fc8b58a17f
casselsg <casselsg19@bcdschool.org>
2018-07-11 20:22:57 +0000

    automatically backed up by learn

--- shopping.rb
+++ shopping.rb
@@ -3,7 +3,10 @@
    shopping_array= ["24K magic CD", "crocs" , "potato", "ACT prep book"]
    price = [15.98,35,0.8, 15]
    
-   shopping_hash= { }
+   shopping_hash= {}
+   items_price = 0
+   shopping_array.each do |each_items|
+   shooping_hash[each_items]=
    
    
-   shooping_array= ["24k nagic CD]
+   
