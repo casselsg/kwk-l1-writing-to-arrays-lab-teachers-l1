8356d178692a0f863d208830dde757c2c197ef1d
casselsg <casselsg19@bcdschool.org>
2018-07-11 20:07:41 +0000

    automatically backed up by learn

---
+++ celebrities.rb
@@ -0,0 +1,22 @@
+
+
+student_celebrities["Megan"]= "Beyonce"
+student_celebrities["Grace"]= "Gustave D."
+student_celebrities["Katie"]= "Miyazaki"
+student_celebrities["Celine"]= "Kylie Jenner"
+
+     # student celebrities << {"Natalie's => y
+     
+     
+     student_celebrities = { } 
+​
+student_celebrities["Megan"] = "Beyonce"
+student_celebrities["Grace"] = "Gustavo D."
+student_celebrities["Katie"] = "Miyazaki"
+student_celebrities["Celine"] = "Kylie Jenner"
+​
+student_names = ["Natalie", "Cece", "Pelumi", "Sidney", "Hope", "Christina","Ella"]
+​
+celebrities = ["Gigi Hadid", "Kendall Jenner", "Sam Smith", "Charlie Puth", "Romme Strigid", "Billie Eilish", "Rihanna"]
+
+
